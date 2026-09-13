74a9bd7905be0dcc72de423b69849c8eecf02aae
davidkmck <108087097+davidkmck@users.noreply.github.com>
2026-09-13 14:35:13 -0400

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'can-expand-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-512.png',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  'https://unpkg.com/@turf/turf@6.5.0/turf.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
